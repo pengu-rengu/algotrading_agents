ee325db7dfbce4a3747658bb5ea8cafa1e335c4e
pengurengu <108484338+pengu-rengu@users.noreply.github.com>
2025-06-14 15:56:26 -0400

    Update fetch_data.jl

--- src/fetch_data.jl
+++ src/fetch_data.jl
@@ -1,6 +1,6 @@
 using HTTP
 
-const api_key = "ASTr9QDuAiOrUygKF0nAiPDZp7PHWDIA"
+const api_key = "alsjfalwkdjf"
 resp = HTTP.get("https://financialmodelingprep.com/stable/historical-price-eod/light?symbol=BTCUSD&apikey=$(api_key)")
 
 open(joinpath(@__DIR__, "response.json"), "w") do file
